81d4fb2c9838192cbff511b5ee4eff19646e32f3
linux_china <libing.chen@gmail.com>
2020-11-25 22:03:34 -0800

    polishing

--- examples/oss-client.rs
+++ examples/oss-client.rs
@@ -1,14 +1,13 @@
 use aliyun_openapi::prelude::*;
-use anyhow::Result;
 use std::str::from_utf8;
-use bytes::Buf;
+use bytes::{Bytes, Buf};
 
 #[tokio::main]
-async fn main() -> Result<()> {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let endpoint = "oss-cn-hangzhou.aliyuncs.com";
     let ref http_client = reqwest::Client::new();
     let oss = OSS { endpoint, http_client };
-    let bytes = oss.get_object("eren-assets", "hello.txt").await?;
+    let bytes: Bytes = oss.get_object("eren-assets", "hello.txt").await?;
     print!("object: {}", from_utf8(bytes.bytes()).unwrap());
     Ok(())
 }
