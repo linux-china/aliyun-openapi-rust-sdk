0d6a8aada4b19a9d87ceb0cd89c15a7e16042124
linux_china <libing.chen@gmail.com>
2020-11-26 14:54:48 -0800

    adjust fields for SimpleEmail

--- src/dm.rs
+++ src/dm.rs
@@ -5,9 +5,9 @@ const API_VERSION: &str = "2015-11-23";
 /// Simple Email
 #[derive(Debug, Default)]
 pub struct SimpleMail<'a> {
-    account_name: &'a str,
+    from: &'a str,
     from_alias: &'a str,
-    to_address: &'a str,
+    to: &'a str,
     subject: &'a str,
     html_body: &'a str,
     text_body: &'a str,
@@ -25,12 +25,12 @@ pub struct DM<'a> {
 impl<'a> DM<'a> {
     pub async fn send(&self, simple_mail: &SimpleMail<'_>) -> reqwest::Result<bool> {
         let mut params: Vec<(&str, &str)> = Vec::new();
-        params.push(("AccountName", simple_mail.account_name));
+        params.push(("AccountName", simple_mail.from));
         params.push(("FromAlias", simple_mail.from_alias));
         params.push(("AddressType", "1"));
         params.push(("ReplyToAddress", "true"));
         params.push(("Subject", simple_mail.subject));
-        params.push(("ToAddress", simple_mail.to_address));
+        params.push(("ToAddress", simple_mail.to));
         params.push(("HtmlBody", simple_mail.html_body));
         let url = sign_url("POST", self.endpoint, API_VERSION,
                            "SingleSendMail", params.as_ref());
@@ -48,10 +48,10 @@ mod tests {
     #[tokio::test]
     async fn test_send() -> Result<(), Box<dyn std::error::Error>> {
         let mail = SimpleMail {
-            account_name: "support@microservices.club",
+            from: "support@microservices.club",
             from_alias: "MicroServicesClub",
             subject: "this is hello",
-            to_address: "libing.chen@gmail.com",
+            to: "libing.chen@gmail.com",
             html_body: "<p>This is hello!</p>",
             text_body: "This is hello!",
         };
