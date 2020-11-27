528668a6f4b3d1ce36814fedad5d6ec8812e5e5d
linux_china <libing.chen@gmail.com>
2020-11-26 16:40:34 -0800

    polishing

--- src/dm.rs
+++ src/dm.rs
@@ -6,11 +6,11 @@ const API_VERSION: &str = "2015-11-23";
 #[derive(Debug, Default)]
 pub struct SimpleMail<'a> {
     /// same as service account on DM console
-    from: &'a str,
+    sender: &'a str,
     /// from alias, such as Customer Support, Admin
-    from_alias: &'a str,
+    sender_alias: &'a str,
     /// receiver email
-    to: &'a str,
+    receiver: &'a str,
     /// email subject
     subject: &'a str,
     /// html body
@@ -31,12 +31,12 @@ pub struct DM<'a> {
 impl<'a> DM<'a> {
     pub async fn send(&self, simple_mail: &SimpleMail<'_>) -> reqwest::Result<bool> {
         let mut params: Vec<(&str, &str)> = Vec::new();
-        params.push(("AccountName", simple_mail.from));
-        params.push(("FromAlias", simple_mail.from_alias));
+        params.push(("AccountName", simple_mail.sender));
+        params.push(("FromAlias", simple_mail.sender_alias));
         params.push(("AddressType", "1"));
         params.push(("ReplyToAddress", "true"));
         params.push(("Subject", simple_mail.subject));
-        params.push(("ToAddress", simple_mail.to));
+        params.push(("ToAddress", simple_mail.receiver));
         params.push(("HtmlBody", simple_mail.html_body));
         let url = sign_url("POST", self.endpoint, API_VERSION,
                            "SingleSendMail", params.as_ref());
@@ -54,10 +54,10 @@ mod tests {
     #[tokio::test]
     async fn test_send() -> Result<(), Box<dyn std::error::Error>> {
         let mail = SimpleMail {
-            from: "support@microservices.club",
-            from_alias: "MicroServicesClub",
+            sender: "support@microservices.club",
+            sender_alias: "MicroServicesClub",
             subject: "this is hello",
-            to: "libing.chen@gmail.com",
+            receiver: "libing.chen@gmail.com",
             html_body: "<p>This is hello!</p>",
             text_body: "This is hello!",
         };
